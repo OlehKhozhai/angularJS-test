3a17a016513d5d6866fda0e887d1a5b03a4e556a
Oleh <o.hozhai@netls.com.ua>
2018-11-06 19:56:59 +0200

    evening

--- angular.js
+++ angular.js
@@ -3,12 +3,21 @@ const app = angular.module('app', []);
 app.controller('firstCtrl', function ($scope, $timeout) {
   $scope.dayDefault = 'Day';
   $scope.monthDefault = 'Month';
-  $scope.monthArray = ['january','february', 'march', 'april', 'may', 'juli', 'julay'];
-
+  $scope.yearDefault = 'Year';
+  $scope.monthArray = {
+    first: ['january', 'february', 'march', 'april', 'may', 'juli', 'julay'],
+    second: ['august', 'september', 'october', 'november', 'december']
+  };
+  console.log($scope.monthArray)
   $scope.dayArray = [];
 
-  for(let i =1; i<=31; i++){
+  for (let i = 1; i <= 31; i++) {
     $scope.dayArray.push(i)
+  };
+
+  $scope.yearArray = [];
+  for (let i = 2008; i >= 1940; i--) {
+    $scope.yearArray.push(i)
   };
 
   $scope.blurClose = function () {
@@ -16,7 +25,7 @@ app.controller('firstCtrl', function ($scope, $timeout) {
       $scope.show = false;
     }, 200);
   };
-  $scope.inValid= false
+  $scope.inValid = false
   $scope.blurClosew = function () {
     $timeout(function () {
       $scope.showw = false;
@@ -27,25 +36,49 @@ app.controller('firstCtrl', function ($scope, $timeout) {
     $scope.dayDefault = dayDefault;
   };
 
+
   $scope.setValue = function (value, text, index) {
-    if(text === 'day'){
+    // switch($scope.monthArray.indexOf($scope.monthDefault)){
+    // case 7: 
+    // }
+    if (text === 'day') {
       $scope.dayDefault = value
-    }
-    if(text === 'month'){
-      $scope.monthDefault = value
-      console.log(index)
-        if(!(index%2 == 0) && ($scope.dayDefault >30)){
-          $scope.inValid= true
-          console.log('!!!')
-        }else{
-          $scope.inValid= false
-        }
-       
-      
+      if (($scope.yearDefault % 4 !== 0) && $scope.monthDefault === 'february' && $scope.dayDefault > 28 && $scope.yearDefault !== 'Year' ||
+        ($scope.yearDefault % 4 === 0) && $scope.monthDefault === 'february' && $scope.dayDefault > 29 && $scope.yearDefault !== 'Year' ||
+        $scope.monthArray.first.indexOf($scope.monthDefault) % 2 !== 0 && $scope.monthArray.second.indexOf($scope.monthDefault) % 2 !== 0 &&
+        ($scope.dayDefault > 30) && $scope.yearDefault !== 'Year') {
+        $scope.inValid = true;
+      } else {
+        $scope.inValid = false;
+      };
+    };
 
+    if (text === 'month') {
+      $scope.monthDefault = value;
+      if (($scope.yearDefault % 4 !== 0) && $scope.monthDefault === 'february' && $scope.dayDefault > 28 && $scope.yearDefault !== 'Year' ||
+        (($scope.monthDefault === 'february') && ($scope.dayDefault > 29)) ||
+        ((index % 2 === 0) && ($scope.dayDefault > 30))) {
+        $scope.inValid = true
+      } else {
+        $scope.inValid = false
+      }
+    };
+
+    if (text === 'year') {
+      $scope.yearDefault = value;
+      if (value % 4 !== 0 && $scope.monthDefault === 'february' && $scope.dayDefault > 28 ||
+        $scope.monthDefault === 'february' && $scope.dayDefault > 29 ||
+        $scope.monthArray.first.indexOf($scope.monthDefault) % 2 !== 0 && $scope.monthArray.second.indexOf($scope.monthDefault) % 2 !== 0 &&
+        $scope.dayDefault > 30) {
+
+
+        $scope.inValid = true
+      } else {
+        $scope.inValid = false
+      }
     }
 
-    
+
   };
 
 });
