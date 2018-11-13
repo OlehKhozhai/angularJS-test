db6247ff183fe855470c1d8eda60cbc074aa78e8
volocuga <tknoleg@gmail.com>
2018-11-13 22:35:01 +0200

    promise

--- angular.js
+++ angular.js
@@ -1,6 +1,6 @@
 const app = angular.module('app', []);
 
-app.controller('firstCtrl', function ($scope, $timeout) {
+app.controller('firstCtrl', function ($scope, $timeout, $q) {
   $scope.dayDefault = 'Day';
   $scope.monthDefault = 'Month';
   $scope.yearDefault = 'Year';
@@ -78,8 +78,86 @@ app.controller('firstCtrl', function ($scope, $timeout) {
         $scope.inValid = false
       }
     }
-
-
   };
+
+  function sum(a, b, callback) {
+    $timeout(function () {
+      callback(a + b);
+    }, 2000)
+  };
+
+  sum(2, 3, function (result) {
+    $scope.result = result
+  })
+
+
+  function sum(a, b) {
+    return $timeout(function () {
+      return (a + b);
+    }, 2000)
+  };
+
+  sum(2, 3)
+    .then(function (result) {
+      $scope.result = result
+    })
+
+
+  $scope.files = [{
+      id: 1,
+      name: 'Oleh'
+    },
+    {
+      id: 2,
+      name: 'pppp'
+    },
+    {
+      id: 3,
+      name: 'Olll'
+    },
+    {
+      id: 4,
+      name: 'Nana'
+    },
+    {
+      id: 5,
+      name: 'Lolo'
+    },
+    {
+      id: 6,
+      name: 'Gygo'
+    },
+  ]
+
+  $scope.i = function (files) {
+    var deferred = $q.defer();
+    var promisee = deferred.promise;
+    promisee.then(() => {
+      files.forEach(good => {
+        $timeout(function () {
+           good.id = 9
+        }, 2000)
+
+       
+      })
+    })
+ console.log('goodssssss', files)
+    deferred.resolve(files);
+    return deferred.promise;
+  }
+
+  $scope.sendPost = function (files) {
+    return $scope.i(files).then(res => {
+      console.log('rerssresresresres', res)
+
+      return $http.post(WP.SHOP_API_DOMAIN + 'api/import/' + $shop.$my().id, {
+          data: data,
+          id:id
+        })
+        .catch(function (err) {
+          err && $log.warn(err);
+        });
+    })
+  }
 
 });
